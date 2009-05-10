ce6a334b599e1f663b16e8a4d0b702c2c510ac29
David Brown <jpool@davidb.org>
2009-05-09 22:14:46 -0800

    Add HashIndex
    The HashIndex maintains a persistent mapping between a set hashes and
    some instance-defined values.
    
    aegis-delta: jpool.1.0.D004
    aegis-uuid: 87928ff5-dde0-42e2-ab35-b9349c6d4911
    aegis-delta-uuid: 83d122d0-d9b6-4b85-9f28-ef12d1977f4a

---
+++ test/org/davidb/jpool/HashIndexSuite.scala
@@ -0,0 +1,178 @@
+//////////////////////////////////////////////////////////////////////
+//
+// The storage pool stores blobs in a series of files (the pool).  The
+// HashIndex keeps track of where in this pool each hash is located.
+//
+// The HashIndex is stored as a series of memory-mapped files, sorted
+// by Hash, containing the hash, and some "extra" information.  The
+// number of files used is a tradeoff between the extra search time
+// (all files have to be searched, as well as the kernel having to
+// maintain extra mappings), and the time needed to rewrite the last
+// file.
+//
+// At any given time, the HashIndex maintains 0 or more mappings of
+// files as well as an internal update of hashes recently written that
+// haven't been flushed out.  See the implementation details below on
+// the Hanoi-style combination to give a decent tradeoff between
+// individual files, and merges that need to be performed.
+//
+// The index is robust against abnormal termination of the program, as
+// long as the underlying filesystem implements reasonable atomic
+// semantics on rename.
+
+package org.davidb.jpool
+
+import collection.{mutable, immutable}
+import java.nio.ByteBuffer
+
+//////////////////////////////////////////////////////////////////////
+// Unit test.
+//////////////////////////////////////////////////////////////////////
+
+import org.scalatest.{Suite, BeforeAndAfter}
+
+class IntHashIndex(val basePath: String, val prefix: String) extends {
+  protected val encoder = new FixedEncodable[Int] {
+    def EBytes = 4
+    def encode(obj: Int, buf: ByteBuffer) {
+      buf.putInt(obj)
+    }
+    def decode(buf: ByteBuffer): Int = {
+      buf.getInt()
+    }
+  }
+} with HashIndex[Int] {
+  // Make this small enough that the test completes in a reasonable
+  // time.  Comment this out for a very long test, that probably
+  // doesn't really exercise anything different, other than that
+  // larger TreeMaps work.
+  override def ramMax = 103
+}
+
+object HashIndexSuite extends Suite {
+  def testInvalidDir {
+    intercept[IllegalArgumentException] {
+      new IntHashIndex("/xxxxx/yyyyy/zzzzz", "index-")
+    }
+  }
+
+  def testPrefixSlash {
+    intercept[IllegalArgumentException] {
+      new IntHashIndex("/tmp", "in/dex-")
+    }
+  }
+
+  def notestIndex {
+    TempDir.withTempDir { name =>
+      val hi = new IntHashIndex(name.getPath, "index-")
+      val a = hi.ramMax * 16 + 1
+      for (i <- 0 until a) {
+        hi += (hashOfNum(i) -> i)
+        if ((i % 1737) == 0)
+          hi.flush()
+      }
+      hi.flush()
+
+      for (i <- 0 until a) {
+        checkNode(hi, i)
+      }
+    }
+  }
+
+  private def checkNode(m: collection.Map[Hash, Int], num: Int) {
+    val h1 = hashOfNum(num)
+    assert(m.get(h1) === Some(num))
+  }
+
+  private def hashOfNum(num: Int): Hash = {
+    Hash("blob", num.toString)
+  }
+}
+
+class HashIndexSuite extends Suite with BeforeAndAfter {
+  var tmpDir: TempDir = null
+  var hashIndex: IntHashIndex = null
+
+  override def beforeEach() {
+    tmpDir = new TempDir
+    hashIndex = new IntHashIndex(tmpDir.path.getPath, "index-")
+  }
+
+  override def afterEach() {
+    tmpDir.close()
+    tmpDir = null
+    hashIndex = null
+  }
+
+  def reload() {
+    hashIndex.close()
+    hashIndex = new IntHashIndex(tmpDir.path.getPath, "index-")
+  }
+
+  // Test the simple case of writing nodes consecutively without any
+  // explicit flushes, except at the end.
+  def testRam {
+    val limit = hashIndex.ramMax * 32 - 1
+    addHashes(0 to limit)
+    validateHashes(0 to limit)
+    hashIndex.flush()
+    validateHashes(0 to limit)
+    reload()
+    validateHashes(0 to limit)
+  }
+
+  // Test reopens across numerous boundaries.
+  def testReopen {
+    val limit = hashIndex.ramMax
+    val bounds = new mutable.ArrayBuffer[Int]
+
+    for (i <- 0 until limit * 16 by limit) {
+      if (i > 0)
+        bounds += i - 1
+      bounds += i
+      bounds += i + 1
+    }
+
+    var lastBound = 0
+    for (bound <- bounds) {
+      // println("Testing bound: " + bound)
+      addHashes(lastBound until bound)
+      validateHashes(0 until bound)
+      reload()
+      if (false && bound == limit * 15 + 1) {
+        Runtime.getRuntime.exec(Array(
+          "/bin/sh", "-c",
+          String.format(
+            "(/bin/ls -l %s;\n" +
+            " for i in %s/*; do\n" +
+            "   echo \"Dump of $i\";\n" +
+            "   hexdump -C $i;\n" +
+            " done) > /tmp/debug",
+            tmpDir.path.getPath, tmpDir.path.getPath))).waitFor()
+      }
+      validateHashes(0 until bound)
+      lastBound = bound
+    }
+  }
+
+  private def addHashes(r: Range) {
+    r.foreach { i => hashIndex.put(bufHash(i), i) }
+  }
+
+  private def validateHashes(r: Range) {
+    r.foreach { i =>
+      // Verify that this hash is present, and "nearby" ones are not.
+      val h = bufHash(i)
+      assert(hashIndex.get(h) === Some(i))
+
+      val bytes = h.getBytes
+      bytes(bytes.length - 1) = (bytes(bytes.length - 1) ^ 1).toByte
+      val h2 = Hash.raw(bytes)
+      assert(hashIndex.get(h2) === None)
+    }
+  }
+
+  private def bufHash(index: Int): Hash = {
+    Hash("blob", index.toString)
+  }
+}
