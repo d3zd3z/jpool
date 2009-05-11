5e539631ad42936a66135319e5b4536d750ab39c
David Brown <jpool@davidb.org>
2009-05-10 17:21:37 -0800

    Create skeletal Pool factory.
    Create skeleton of the pool factory. This factory takes URI's
    indicating access to a specific pool, and returns a pool handler for
    that URI. This initial version contains no pools, so only generates
    errors.
    
    aegis-delta: jpool.1.0.D010
    aegis-uuid: d7276a2a-f6ed-4036-ba7c-80ec73795f9c
    aegis-delta-uuid: 2d5385d7-54c2-41f7-8300-0b71d474e65e

---
+++ src/org/davidb/jpool/pool/ChunkSource.scala
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////
+// A ChunkSource is somethign that contains chunks, and can retrieve
+// them.
+
+package org.davidb.jpool.pool
+
+trait ChunkSource {
+  // Retrieve all of the backups available in this source.
+  def getBackups: Set[Hash]
+
+  // Attempt to read a single Chunk from this pool.
+  def readChunk(hash: Hash): Option[Chunk]
+}
