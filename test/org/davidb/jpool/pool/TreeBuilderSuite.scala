e90bc8eea536283684a95fa0d4bcd992089c3653
David Brown <jpool@davidb.org>
2009-05-17 16:40:23 -0800

    Squash indirect chunks with only one hash.
    It is not necessary to store an indirect chunk that contains only
    a single hash.	Collapse these by just using the hash stored in
    the chunks directly.
    
    aegis-delta: jpool.1.0.D023
    aegis-uuid: c49b1775-32e0-4dee-a3d3-f6596a813028

--- test/org/davidb/jpool/pool/TreeBuilderSuite.scala
+++ test/org/davidb/jpool/pool/TreeBuilderSuite.scala
@@ -7,16 +7,21 @@ import org.scalatest.{Suite, BeforeAndAfter}
 
 class TreeBuilderSuite extends Suite with PoolTest {
 
-  def testSingle = rangeCheck(1 to 1)
+  def testSingle = {
+    val hash = rangeCheck(1 to 1)
+    checkKind(hash, "blob")
+  }
   def testTwo = rangeCheck(1 to 2)
   def testTen = rangeCheck(1 to 10)
   def testEleven = rangeCheck(1 to 11)
   def testMany = rangeCheck(1 to 1111)
 
-  private def rangeCheck(nums: Range) {
+  private def rangeCheck(nums: Range): Hash = {
     val tb = TreeBuilder.makeBuilder("tmp", pool, 10*Hash.HashLength)
     add(tb, nums)
-    check(tb.finish(), nums)
+    val hash = tb.finish()
+    check(hash, nums)
+    hash
   }
 
   private def add(tb: TreeBuilder, nums: Range) {
@@ -29,5 +34,10 @@ class TreeBuilderSuite extends Suite with PoolTest {
     val walker = TreeBuilder.walk("tmp", pool, hash)
     val expected = nums map (makeChunk(_, 1))
     assert ((walker map (_.hash)) sameElements (expected map (_.hash)))
+  }
+
+  private def checkKind(hash: Hash, kind: String) {
+    val chunk = pool(hash)
+    assert(chunk.kind === kind)
   }
 }
