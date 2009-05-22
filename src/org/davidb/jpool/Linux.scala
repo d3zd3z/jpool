607433f095d38f2b55a5a411e3cf75a623924df7
David Brown <jpool@davidb.org>
2009-05-22 15:53:07 -0800

    Native 'lstat' call.
    Extract stat information from a given file and build it into a set of
    portable properties.
    
    aegis-delta: jpool.1.0.D031
    aegis-uuid: c74ed779-f356-445d-8d6a-ec7eeab8c30e
    aegis-delta-uuid: 536031e8-bb6e-4b0b-9c30-689e44477341

--- src/org/davidb/jpool/Linux.scala
+++ src/org/davidb/jpool/Linux.scala
@@ -4,6 +4,7 @@
 package org.davidb.jpool
 
 import java.io.IOException
+import scala.collection.immutable
 
 object Linux {
   @native def setup
@@ -14,12 +15,26 @@ object Linux {
   @native
   def readDir(name: String): List[(String, Long)]
 
+  @native
+  def lstat(name: String): Map[String, String]
+
   // These wrappers keep the JNI code less dependent on the
   // implementation details of the Scala runtime.
   private def makePair(name: String, inum: Long) = (name, inum)
 
+  // For the mapping, these functions provide the zero and mplus
+  // operations.
+  private def infoZero = immutable.Map.empty
+  private def infoPlus(prior: immutable.Map[String, String], key: String, value: String):
+    immutable.Map[String, String] =
+  {
+    prior + (key -> value)
+  }
+
   private def readdirError(path: String, errno: Int): Nothing =
     throw new IOException("Error reading directory: '%s' (%d)" format (path, errno))
+  private def lstatError(path: String, errno: Int): Nothing =
+    throw new IOException("Error statting node: '%s' (%d)" format (path, errno))
 
   System.loadLibrary("linux")
   setup
--- test/org/davidb/jpool/LinuxSuite.scala
+++ test/org/davidb/jpool/LinuxSuite.scala
@@ -24,6 +24,28 @@ class LinuxSuite extends Suite {
     }
   }
 
+  def testBadStat {
+    intercept[IOException] {
+      Linux.lstat("/invalidDir/stuff")
+    }
+  }
+
+  private def checkName(name: String, kind: String) {
+    val stat = Linux.lstat(name)
+    // printf("lstat(%s): %s%n", name, stat)
+    checkStat(name, stat, kind)
+  }
+
+  def testStats {
+    // TODO: Device nodes and such aren't that portable.
+    checkName("/bin/ls", "REG")
+    checkName("/dev/null", "CHR")
+    checkName("/dev/sda", "BLK")
+    checkName(".", "DIR")
+    // TODO: Once we have code to make these, test them.
+    // checkName("/etc/make.profile", "LNK")
+  }
+
   // Duplicate the native readdir, using find.
   private def findReadDir(path: String): List[(String, Long)] = {
     val proc = new ProcessBuilder("find", path, "-maxdepth", "1", "-printf", "%P\\t%i\\n").start
@@ -48,6 +70,34 @@ class LinuxSuite extends Suite {
     run()
     assert(proc.waitFor() === 0)
     result.toList
+  }
+
+  private def checkStat(path: String, info: Map[String, String], kind: String) {
+    assert(info("*kind*") === kind)
+    val proc = new ProcessBuilder("stat", "--format", "%s %Y %Z %h %u %g %f %i %d %t %T", path).start
+    drainError(proc)
+    val procOut = new BufferedReader(new InputStreamReader(proc.getInputStream))
+    val line = procOut.readLine()
+    var tmp = line
+    while (tmp ne null) {
+      tmp = procOut.readLine()
+    }
+    assert(line ne null)
+    val fields = line.split(" ")
+    def hexField(n: Int) = java.lang.Long.parseLong(fields(n), 16)
+    // printf("fields: %s", fields.toList)
+    assert(fields(0) === info("size"))
+    assert(fields(1) === info("mtime"))
+    assert(fields(2) === info("ctime"))
+    assert(fields(3) === info("nlink"))
+    assert(fields(4) === info("uid"))
+    assert(fields(5) === info("gid"))
+    assert((hexField(6) & 07777).toString === info("mode"))
+    assert(fields(7) === info("ino"))
+    assert(fields(8) === info("dev"))
+    if (kind == "CHR" || kind == "BLK") {
+      assert((hexField(9) * 256 + hexField(10)).toString === info("rdev"))
+    }
   }
 
   private def drainError(proc: Process) {
