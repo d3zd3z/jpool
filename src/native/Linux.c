a261c72484321d77ef21ec711d6ec2f749323f0f
David Brown <jpool@davidb.org>
2009-05-22 15:53:07 -0800

    Native 'lstat' call.
    Extract stat information from a given file and build it into a
    set of portable properties.
    
    aegis-delta: jpool.1.0.D031
    aegis-uuid: c74ed779-f356-445d-8d6a-ec7eeab8c30e

--- src/native/Linux.c
+++ src/native/Linux.c
@@ -17,6 +17,9 @@
 
 static jmethodID makePair;
 static jmethodID readdirError;
+static jmethodID lstatError;
+static jmethodID infoZero;
+static jmethodID infoPlus;
 
 static jclass ListBufferClass;
 static jmethodID lbCtor;
@@ -37,6 +40,22 @@ JNIEXPORT void JNICALL Java_org_davidb_jpool_Linux_00024_setup
 	readdirError = (*env)->GetMethodID(env, clazz, "readdirError",
 			"(Ljava/lang/String;I)Lscala/runtime/Nothing$;");
 	if (readdirError == NULL)
+		return;
+
+	lstatError = (*env)->GetMethodID(env, clazz, "lstatError",
+			"(Ljava/lang/String;I)Lscala/runtime/Nothing$;");
+	if (lstatError == NULL)
+		return;
+
+	infoZero = (*env)->GetMethodID(env, clazz, "infoZero",
+			"()Lscala/collection/immutable/Map;");
+	if (infoZero == NULL)
+		return;
+
+	infoPlus = (*env)->GetMethodID(env, clazz, "infoPlus",
+			"(Lscala/collection/immutable/Map;Ljava/lang/String;Ljava/lang/String;)"
+			"Lscala/collection/immutable/Map;");
+	if (infoPlus == NULL)
 		return;
 
 	tmp = (*env)->FindClass(env, "scala/collection/mutable/ListBuffer");
@@ -125,6 +144,128 @@ JNIEXPORT jobject JNICALL Java_org_davidb_jpool_Linux_00024_readDir
 failure:
 	closedir(dirp);
 	return NULL;
+}
+
+/* Set a property. */
+static jobject setProp(JNIEnv *env, jobject obj, jobject map, char *key, char *val)
+{
+	jobject ks, vs;
+	ks = (*env)->NewStringUTF(env, key);
+	if (ks == NULL)
+		return NULL;
+	vs = (*env)->NewStringUTF(env, val);
+	if (vs == NULL)
+		return NULL;
+	jobject nmap = (*env)->CallObjectMethod(env, obj, infoPlus, map, ks, vs);
+	(*env)->DeleteLocalRef(env, ks);
+	(*env)->DeleteLocalRef(env, vs);
+	(*env)->DeleteLocalRef(env, map);
+	return nmap;
+}
+
+JNIEXPORT jobject JNICALL Java_org_davidb_jpool_Linux_00024_lstat
+	(JNIEnv *env, jobject obj, jstring path)
+{
+	const int len = (*env)->GetStringLength(env, path);
+	char buf[len+1];
+	int i;
+	const jchar *src = (*env)->GetStringChars(env, path, NULL);
+	for (i = 0; i < len; i++)
+		buf[i] = src[i];
+	buf[len] = 0;
+	(*env)->ReleaseStringChars(env, path, src);
+
+	struct stat sbuf;
+	int res = lstat(buf, &sbuf);
+
+	if (res < 0) {
+		(*env)->CallObjectMethod(env, obj, lstatError, path, (jint) errno);
+		return NULL;
+	}
+
+	jobject map = (*env)->CallObjectMethod(env, obj, infoZero);
+
+	/* Determine the kind. */
+	char *v;
+	if (S_ISREG(sbuf.st_mode))
+		v = "REG";
+	else if (S_ISDIR(sbuf.st_mode))
+		v = "DIR";
+	else if (S_ISCHR(sbuf.st_mode))
+		v = "CHR";
+	else if (S_ISBLK(sbuf.st_mode))
+		v = "BLK";
+	else if (S_ISFIFO(sbuf.st_mode))
+		v = "FIFO";
+	else if (S_ISLNK(sbuf.st_mode))
+		v = "LNK";
+	else if (S_ISSOCK(sbuf.st_mode))
+		v = "SOCK";
+	else if (S_ISSOCK(sbuf.st_mode))
+		v = "DIR";
+	else {
+		(*env)->CallObjectMethod(env, obj, lstatError, path, EINVAL);
+		return NULL;
+	}
+	map = setProp(env, obj, map, "*kind*", v);
+	if (map == NULL)
+		return;
+
+	char tmp[40];
+
+	sprintf(tmp, "%lld", (long long)(sbuf.st_mode & (~S_IFMT)));
+	map = setProp(env, obj, map, "mode", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_dev);
+	map = setProp(env, obj, map, "dev", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_ino);
+	map = setProp(env, obj, map, "ino", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_nlink);
+	map = setProp(env, obj, map, "nlink", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_uid);
+	map = setProp(env, obj, map, "uid", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_gid);
+	map = setProp(env, obj, map, "gid", tmp);
+	if (map == NULL)
+		return;
+
+	if (S_ISCHR(sbuf.st_mode) || S_ISBLK(sbuf.st_mode)) {
+		sprintf(tmp, "%lld", (long long)sbuf.st_rdev);
+		map = setProp(env, obj, map, "rdev", tmp);
+		if (map == NULL)
+			return;
+	}
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_size);
+	map = setProp(env, obj, map, "size", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_mtime);
+	map = setProp(env, obj, map, "mtime", tmp);
+	if (map == NULL)
+		return;
+
+	sprintf(tmp, "%lld", (long long)sbuf.st_ctime);
+	map = setProp(env, obj, map, "ctime", tmp);
+	if (map == NULL)
+		return;
+
+	return map;
 }
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved)
