2f7b011effa4a84d241555eee365f68b900fb6ed
David Brown <jpool@davidb.org>
2009-05-22 20:57:03 -0800

    Write bulk file data.
    Native interface to write bulk data to a file.
    
    aegis-delta: jpool.1.0.D036
    aegis-uuid: 52f879a3-1714-4afc-8a80-0846d7d4458d
    aegis-delta-uuid: f4247fe2-25c2-47f0-97cd-7eae3f69b327

--- src/org/davidb/jpool/Linux.scala
+++ src/org/davidb/jpool/Linux.scala
@@ -10,26 +10,38 @@ import scala.collection.immutable
 object Linux {
   @native def setup
 
-  @native
-  def message: String
+  @native def message: String
 
-  @native
-  def readDir(name: String): List[(String, Long)]
+  @native def readDir(name: String): List[(String, Long)]
 
-  @native
-  def lstat(name: String): Map[String, String]
+  @native def lstat(name: String): Map[String, String]
 
-  @native
-  def readlink(name: String): String
+  @native def readlink(name: String): String
 
-  @native
-  def symlink(oldPath: String, newPath: String)
+  @native def symlink(oldPath: String, newPath: String)
 
   // Bulk read.  Reads the contents of the given file, in blocks of
   // the given chunks size, and calls 'process' with a fresh
   // ByteBuffer for each chunk.
-  @native
-  def readFile(path: String, chunkSize: Int, process: ByteBuffer => Unit)
+  @native def readFile(path: String, chunkSize: Int, process: ByteBuffer => Unit)
+
+  // Bulk write.  The iterator should return ByteBuffers, the contents
+  // of which will be written to the file.
+  def writeFile(path: String, chunks: Iterator[ByteBuffer]) {
+    val fd = openForWrite(path)
+    try {
+      for (chunk <- chunks) {
+        writeChunk(fd, chunk.array, chunk.arrayOffset + chunk.position,
+          chunk.remaining)
+      }
+    } finally {
+      close(fd)
+    }
+  }
+
+  @native protected def openForWrite(path: String): Int
+  @native protected def close(fd: Int)
+  @native protected def writeChunk(fd: Int, data: Array[Byte], offset: Int, length: Int)
 
   // These wrappers keep the JNI code less dependent on the
   // implementation details of the Scala runtime.
@@ -53,9 +65,15 @@ object Linux {
   private def symlinkError(oldPath: String, newPath: String, errno: Int): Nothing =
     throw new IOException("Error making symlink from '%s' to '%s' (%d)"
       format (oldPath, newPath, errno))
+  private def openError(path: String, errno: Int): Nothing =
+    throw new IOException("Error opening file: '%s' (%d)" format (path, errno))
   private def readError(path: String, errno: Int): Nothing =
     throw new IOException("Error reading file '%s' (%d)"
       format (path, errno))
+  private def writeError(errno: Int): Nothing =
+    throw new IOException("Error writing file (%d)" format errno)
+  private def closeError(errno: Int): Nothing =
+    throw new IOException("Error closing file (%d)" format errno)
 
   System.loadLibrary("linux")
   setup
