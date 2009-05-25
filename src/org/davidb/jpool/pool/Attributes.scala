47a3c2baab4b9792cde2d73151aec490960f469b
David Brown <jpool@davidb.org>
2009-05-24 23:12:19 -0800

    Encode and decode attributes.
    Encode/decode the file object attributes into an XML format and then
    into a stream that can be stored into the pool.
    
    aegis-delta: jpool.1.0.D048
    aegis-uuid: bc08bf23-8e18-45be-99ec-97e992c9257a
    aegis-delta-uuid: 012d0ee5-2159-46db-8fc9-ee04524caad8

---
+++ src/org/davidb/jpool/pool/Attributes.scala
@@ -0,0 +1,88 @@
+//////////////////////////////////////////////////////////////////////
+// File attributes.
+//
+// These are a lot like properties, except fit better into Scala, and
+// have a "kind", and "name" property associated with them.
+//
+// Note that name fields and such are encoded into UTF-8 even though
+// they come from byte encoded Linux filenames.  This means that
+// Unicode characters will not be represented correctly in the UTF-8,
+// but the conversion will be reversable, and allows the malformed
+// filenames allowed under Linux.
+
+package org.davidb.jpool.pool
+
+import org.apache.commons.codec.binary.Base64
+import scala.collection.mutable.ListBuffer
+import scala.xml
+import java.io.{ByteArrayOutputStream, OutputStreamWriter}
+
+object Attributes {
+  def decode(bytes: Array[Byte]): Attributes = {
+    val nodes = xml.XML.loadString(new String(bytes, "UTF-8"))
+    val kind = (nodes \ "@kind").text
+    val name = (nodes \ "@name").text
+    val entries = for (child <- nodes \ "entry") yield decodeEntry(child)
+    new Attributes(kind, name, Map[String, String]() ++ entries)
+  }
+
+  private def decodeEntry(node: xml.Node): (String, String) = {
+    val key = node \ "@key"
+    val body = node.text
+    if ((node \ "@encoded").length > 0) {
+      val decoded = new String(Base64.decodeBase64(body.getBytes()), "iso8859-1")
+      (key.text, decoded)
+    } else {
+      (key.text, body)
+    }
+  }
+}
+
+class Attributes(var kind: String, var name: String,
+  private val contents: Map[String, String]) extends scala.collection.Map[String, String]
+{
+  // Map interface.
+  def size: Int = contents.size
+  def get(key: String): Option[String] = contents.get(key)
+  def elements = contents.elements
+
+  // Convert this node into XML.
+  def toXML: xml.Elem = {
+    val items = for (key <- contents.keys.toList.sort(_<_))
+      yield entryEncode(key, contents(key))
+    <node kind={kind} name={name}>{items}</node>
+  }
+
+  // Encode this entry, using base-64 if necessary.  Also validates
+  // that every character fits within a byte.  Note that a space will
+  // cause us to use base64, since a space at the end or beginning of
+  // a string might easily be eaten.
+  private def entryEncode(key: String, value: String): xml.Elem = {
+    if (value exists (ch => ch.toInt > 255))
+      error("Encoding error, non 8-bit character")
+    if (value forall (ch => ch >= '!' && ch <= '~')) {
+      <entry key={key}>{value}</entry>
+    } else {
+      val encoded = new String(Base64.encodeBase64(value.getBytes("iso8859-1")))
+      <entry key={key} encoded="base64">{encoded}</entry>
+    }
+  }
+
+  // Convert this node into a Binary representation of the XML.
+  def toByteArray(): Array[Byte] = {
+    val buf = new StringBuilder
+    buf.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    buf.append(xml.Utility.toXML(toXML))
+    buf.toString.getBytes("UTF-8")
+  }
+
+  override def toString(): String = xml.Utility.toXML(toXML)
+
+  override def equals(that: Any): Boolean = that match {
+    case other: Attributes =>
+      kind == other.kind &&
+        name == other.name &&
+        contents == other.contents
+    case _ => false
+  }
+}
---
+++ test/org/davidb/jpool/pool/AttributesSuite.scala
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////
+// Testing of attribute stores.
+
+package org.davidb.jpool.pool
+
+import org.scalatest.Suite
+
+class AttributesSuite extends Suite {
+  def testXML {
+    var map = Map[String, String]()
+    map += ("aaa" -> "123")
+    map += ("bbb" -> "456")
+    map += ("ugly" -> "This\370is\201ugly")
+    val att = new Attributes("SILLY", "silly attribute set", map)
+    val bin = att.toByteArray()
+    // Pdump.dump(bin)
+
+    val attb = Attributes.decode(bin)
+    assert(att === attb)
+  }
+
+  // Test that we can encode and decode all possible characters.
+  def testChars {
+    var map = Map[String, String]()
+    for (key <- 0 until 256) {
+      map += (key.toString -> new String(Array(key.toChar)))
+    }
+    val att = new Attributes("Kind", "this is my name", map)
+    val bin = att.toByteArray()
+    // Pdump.dump(bin)
+    val attb = Attributes.decode(bin)
+
+    /*
+    for (keyb <- 0 until 256) {
+      val key = keyb.toString
+      if (!(attb contains key)) {
+        printf("Missing key %d%n", keyb)
+      } else if (att(key) != attb(key)) {
+        printf("Mismatch on key: %s '%s' '%s'%n", key, att(key), attb(key))
+      }
+    }
+    */
+    assert(att === attb)
+  }
+}
