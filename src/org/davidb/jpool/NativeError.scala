26b95c457e516ab3809fc38aaa7c7ca8c0978e90
David Brown <jpool@davidb.org>
2009-05-24 16:31:04 -0800

    Factor Linux out to NativeError.
    The subclass is causing parallel compilation problems.
    
    aegis-delta: jpool.1.0.D046
    aegis-uuid: a64862f3-8b89-4c71-aae0-bc4348afcdb3

---
+++ src/org/davidb/jpool/NativeError.scala
@@ -0,0 +1,9 @@
+//////////////////////////////////////////////////////////////////////
+// Native errors.
+
+package org.davidb.jpool
+
+import java.io.IOException
+
+class NativeError(val name: String, val path: String, val errno: Int, message: String)
+  extends IOException(message)
