373fd10cee05e737ad47026184c6e92c072ac0f8
David Brown <jpool@davidb.org>
2009-05-29 21:08:29 -0800

    Have restore check the kind.
    Restore appropriately depending on the specified kind.
    
    aegis-delta: jpool.1.0.D058
    aegis-uuid: ff1daaf0-97fd-4530-b7fa-d06444f485b0

--- src/org/davidb/jpool/tools/Restore.scala
+++ src/org/davidb/jpool/tools/Restore.scala
@@ -8,20 +8,41 @@ package org.davidb.jpool.tools
 
 import java.net.URI
 import java.nio.channels.Channels
-import org.davidb.jpool.pool.{TarRestore, PoolFactory}
+import org.davidb.jpool.pool.{Back, TarRestore, PoolFactory}
 
-object Restore {
+import org.davidb.logging.Logger
+
+object Restore extends AnyRef with Logger {
   def main(args: Array[String]) {
-    if (args.length != 2) {
-      System.err.println("Usage: Restore jpool:file:///path hash > tarball")
-      System.exit(1)
+    if (args.length != 3) {
+      logError("Usage: Restore jpool:file:///path hash --tar > tarball")
+      logError("               jpool:file:///path hash path")
+      exit(1)
     }
 
     val pool = PoolFactory.getInstance(new URI(args(0)))
-    val stdout = Channels.newChannel(System.out)
-    val restorer = new TarRestore(pool, stdout)
-    restorer.decode(Hash.ofString(args(1)))
-    restorer.finish()
+    val hash = Hash.ofString(args(1))
+    val back = Back.load(pool, hash)
+    if (args(2) == "--tar") {
+      // Tar snapshots don't have a kind property, or it should be set to
+      // 'tar' if it does have one.
+      if (back.props.getProperty("kind", "tar") != "tar") {
+        logError("Specified --tar, but indicate backup hash is not a tar snapshot")
+        exit(1)
+      }
+      val stdout = Channels.newChannel(System.out)
+      val restorer = new TarRestore(pool, stdout)
+      restorer.decode(hash)
+      restorer.finish()
+    } else {
+      // Otherwise, this should be a 'snapshot' backup.
+      if (back.props.getProperty("kind") != "snapshot") {
+        logError("Specified backup is not a snapshot backup")
+        exit(1)
+      }
+
+      logError("TODO: Implement restore of snapshot backups")
+    }
     pool.close
   }
 }
