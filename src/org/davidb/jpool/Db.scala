539c096b95659de81f27898b4633b1476fb8fb60
David Brown <jpool@davidb.org>
2009-05-30 14:35:36 -0800

    Factor general DB code out.
    Make the Pool DB code specialized, factoring out the general DB code
    into it's own class.
    
    aegis-delta: jpool.1.0.D064
    aegis-uuid: 4f0542c7-35d5-4d38-94f4-d66cf69b8b6c
    aegis-delta-uuid: 37d374d5-f962-4e6e-bc0c-d66efa8e4e4d

---
+++ src/org/davidb/jpool/Db.scala
@@ -0,0 +1,150 @@
+//////////////////////////////////////////////////////////////////////
+// General database access.
+
+package org.davidb.jpool
+
+import java.sql.{Connection, Driver, ResultSet, PreparedStatement}
+
+import org.davidb.logging.Logger
+
+// Each database has a particular schema, and potentially the ability
+// to upgrade schema versions.
+trait DbSchema {
+  // A string that describes the version of this schema.
+  val schemaVersion: String
+
+  // The SQL statements that will create this schema in a blank
+  // database.
+  // Note that the Db code itself will create a table called
+  // 'properties' mapping strings to strings.
+  val schemaText: Array[String]
+
+  // Attempt to upgrade this database to the latest schema version,
+  // given the old version.  Can raise an exception if the upgrade is
+  // not possible.
+  def schemaUpgrade(oldVersion: String, db: Db)
+}
+
+class Db(path: String, schema: DbSchema) extends AnyRef with Logger {
+  private val conn = connectDatabase
+  conn.setAutoCommit(false)
+  checkSchema
+
+  def close() = conn.close()
+  def commit() = conn.commit()
+
+  // Perform a query on the database that expects no results.
+  def updateQuery(sql: String, args: Any*) {
+    val stmt = makeQuery(sql, args: _*)
+    stmt.executeUpdate()
+    stmt.close()
+  }
+
+  // Perform a query on the database.  Returns an iterator over the
+  // result set.
+  def query[A](convert: ResultSet => A, sql: String, args: Any*): Iterator[A] = {
+    val stmt = makeQuery(sql, args: _*)
+    val result = stmt.executeQuery()
+    iterateResult(convert, result,  stmt)
+  }
+
+  def getProperty(key: String, default: String): String = {
+    query(getString1 _, "select value from properties where key = ?", key).toList match {
+      case List(item) => item
+      case Nil => default
+      case _ => error("Multiple property entries for %s in properties table" format key)
+    }
+  }
+
+  // This does _not_ auto commit.
+  def setProperty(key: String, value: String) {
+    updateQuery("delete from properties where key = ?", key)
+    updateQuery("insert into properties values (?, ?)", key, value)
+  }
+
+  private def connectDatabase: Connection = {
+    Class.forName("org.h2.Driver")
+    val url = "jdbc:h2:" + path
+    val user = "sa"
+    val pass = ""
+    java.sql.DriverManager.getConnection(url, user, pass)
+  }
+
+  // Useful conversion functions.
+  def getString1(rs: ResultSet): String = rs.getString(1)
+
+  private def checkSchema {
+    val goodVersion = schema.schemaVersion
+    if (tablePresent("PROPERTIES")) {
+      getProperty("schema-version", null) match {
+        case oldVersion if oldVersion == goodVersion =>
+        case null => error("TODO: Handle corrupt database (missing version)")
+        case oldVersion => error("TODO: Implement Schema upgrade")
+      }
+    } else {
+      setSchema(goodVersion)
+    }
+  }
+
+  private def setSchema(version: String) {
+    warn("Creating database: %s", path)
+    schema.schemaText.foreach(updateQuery(_))
+    updateQuery("create table properties (key varchar not null primary key, value varchar)")
+    setProperty("schema-version", version)
+    commit()
+  }
+
+  // Turn a ResultSet into an iterator, calling the convert on it.  It
+  // is important to not use the ResultSet beyond the invocation of
+  // convert, since the iterator will have already advanced or even
+  // closed it.
+  private def iterateResult[A](convert: ResultSet => A,
+    result: ResultSet, stmt: PreparedStatement): Iterator[A] =
+  {
+    // It is necessary to pre-fetch the results, because the ResultSet
+    // class has weird behavior on empty results.
+    new Iterator[A] {
+      var item: Option[A] = _
+      private def advance {
+        item = if (result.next()) Some(convert(result)) else None
+        if (item eq None)
+           stmt.close()
+      }
+      advance
+
+      def hasNext: Boolean = item != None
+      def next: A = {
+        item match {
+          case None => error("next on empty iterator")
+          case Some(r) =>
+            val result = r
+            advance
+            result
+        }
+      }
+    }
+  }
+
+  private def makeQuery(sql: String, args: Any*): PreparedStatement = {
+    val stmt = conn.prepareStatement(sql)
+    var pos = 0
+    while (pos < args.size) {
+      args(pos) match {
+        case b: Array[Byte] => stmt.setBytes(pos + 1, b)
+        case s: String => stmt.setString(pos + 1, s)
+        case i: Int => stmt.setInt(pos + 1, i)
+        case _ => error("Unsupported type for SQL statement")
+      }
+      pos += 1
+    }
+    stmt
+  }
+
+  private def tablePresent(name: String): Boolean = {
+    val md = conn.getMetaData()
+    val res = md.getTables(null, null, name, Array("TABLE"))
+    val answer = res.first()
+    res.close()
+    answer
+  }
+}
--- src/org/davidb/jpool/pool/PoolDb.scala
+++ src/org/davidb/jpool/pool/PoolDb.scala
@@ -12,156 +12,51 @@ import java.util.UUID
 // To start with, let's just port the existing code over.  We can
 // extend this later.
 
-class PoolDb(metaPrefix: File) {
-  private final val schema = Array(
-    "create table config (key varchar not null primary key, value varchar)",
+object PoolDbSchema extends DbSchema {
+  val schemaVersion = "1:2009-05-30"
+  val schemaText = Array(
     "create table backups (hash binary(20) not null primary key)")
+  def schemaUpgrade(oldVersion: String, db: Db) = error("TODO: Upgrade")
+}
 
-  private val conn = connectDatabase
-  conn.setAutoCommit(false)
-  checkSchema
+class PoolDb(metaPrefix: File) {
+  private val db = new Db(new File(metaPrefix, "meta").getAbsolutePath(), PoolDbSchema)
+
   val uuid = checkUUID
 
-  // These are the various types of fields we can store or retrieve.
-  def close() = conn.close()
+  // Passthroughs to the underlying database.
+  def close() = db.close()
+  def getProperty(key: String, default: String) = db.getProperty(key, default)
+  def setProperty(key: String, value: String) {
+    db.setProperty(key, value)
+    db.commit()
+  }
 
   // Add a record that the specified backup is present.
   def addBackup(hash: Hash) {
     val b = hash.getBytes()
-    updateQuery("delete from backups where hash = ?", b)
-    updateQuery("insert into backups values (?)", b)
-    conn.commit()
+    db.updateQuery("delete from backups where hash = ?", b)
+    db.updateQuery("insert into backups values (?)", b)
+    db.commit()
   }
 
   def getBackups(): Set[Hash] = {
     var result = Set[Hash]()
     def getHash1(rs: ResultSet): Hash = Hash.raw(rs.getBytes(1))
-    for (hash <- query(getHash1 _, "select hash from backups"))
+    for (hash <- db.query(getHash1 _, "select hash from backups")) {
       result += hash
+    }
     result
   }
 
-  // Retrieve a property from the config table.
-  def getProperty(key: String, default: String): String = {
-    query(getString1 _, "select value from config where key = ?", key).toList match {
-      case List(item) => item
-      case Nil => default
-      case _ => error("Multiple config entries for %s in config table" format key)
-    }
-  }
-
-  // Set and/or replace a property in the config table.
-  def setProperty(key: String, value: String) {
-    updateQuery("delete from config where key = ?", key)
-    updateQuery("insert into config values (?, ?)", key, value)
-    conn.commit()
-  }
-
-  private def makeQuery(sql: String, args: Any*): PreparedStatement = {
-    val stmt = conn.prepareStatement(sql)
-    var pos = 0
-    while (pos < args.size) {
-      args(pos) match {
-        case b: Array[Byte] => stmt.setBytes(pos + 1, b)
-        case s: String => stmt.setString(pos + 1, s)
-        case _ => error ("Unsupported type for SQL statement")
-      }
-      pos += 1
-    }
-    stmt
-  }
-
-  private def updateQuery(sql: String, args: Any*) {
-    val stmt = makeQuery(sql, args: _*)
-    stmt.executeUpdate()
-    stmt.close()
-  }
-
-  private def query[A](convert: ResultSet => A,
-    sql: String, args: Any*): Iterator[A] =
-  {
-    val stmt = makeQuery(sql, args: _*)
-    val result = stmt.executeQuery()
-    // It is necessary to pre-fetch the results, because ResultSet
-    // considers an empty set to not be on the last entry.
-    new Iterator[A] {
-      var item: Option[A] = _
-      private def advance {
-        item = if (result.next()) Some(convert(result)) else None
-      }
-      advance
-
-      def hasNext: Boolean = item != None
-      def next: A = {
-        item match {
-          case None => error("next on empty iterator")
-          case Some(r) =>
-            val result = r
-            advance
-            result
-        }
-      }
-    }
-  }
-  private def getString1(rs: ResultSet): String = rs.getString(1)
-
-  private def connectDatabase: Connection = {
-    Class.forName("org.h2.Driver")
-    val url = "jdbc:h2:" + new File(metaPrefix, "meta").getAbsolutePath()
-    val user = "sa"
-    val pass = ""
-    java.sql.DriverManager.getConnection(url, user, pass)
-  }
-
-  private def checkSchema {
-    val hash = getSchemaHash
-    if (tablePresent("CONFIG")) {
-      query(getString1, "select value from config where key = 'schema-hash'").toList match {
-        case List(dbHash) if hash == dbHash =>
-        case List(dbHash) => error("TODO: Implement Schema upgrade")
-        case _ => error("TODO: Handle corrupt database (missing hash)")
-      }
-    } else {
-      setSchema(hash)
-    }
-  }
-
-  private def setSchema(hash: String) {
-    println("Creating table")
-    schema.foreach(updateQuery(_))
-
-    updateQuery("insert into config values (?, ?)", "schema-hash", hash)
-    conn.commit()
-  }
-
-  private def getSchemaHash: String = {
-    val md = MessageDigest.getInstance("SHA-1")
-    for (line <- schema) {
-      md.update(line.getBytes("UTF-8"))
-      md.update(';'.toByte)
-    }
-    val digest = md.digest()
-    val result = new StringBuilder(digest.size * 2)
-    for (b <- digest) {
-      result.append("%02x".format(b & 0xff))
-    }
-    result.toString
-  }
-
   private def checkUUID: UUID = {
-    // TODO: Use query.
-    val stmt = conn.prepareStatement("select value from config where key = 'uuid'")
-    val rs = stmt.executeQuery()
-    if (rs.first()) {
-      val uuid = UUID.fromString(rs.getString(1))
-      stmt.close()
-      uuid
-    } else {
-      val uuid = (legacyUUID orElse Some(UUID.randomUUID())).get
-      stmt.close()
-      updateQuery("insert into config values ('uuid', ?)", uuid.toString)
-      conn.commit()
-      uuid
+    db.getProperty("uuid", null) match {
+      case null =>
+        val uuid = (legacyUUID orElse Some(UUID.randomUUID())).get
+        db.setProperty("uuid", uuid.toString)
+        db.commit()
+        uuid
+      case uuidText => UUID.fromString(uuidText)
     }
   }
 
@@ -192,13 +87,5 @@ class PoolDb(metaPrefix: File) {
       else
         None
     } else None
-  }
-
-  private def tablePresent(name: String): Boolean = {
-    val md = conn.getMetaData()
-    val res = md.getTables(null, null, name, Array("TABLE"))
-    val answer = res.first()
-    res.close()
-    answer
   }
 }
