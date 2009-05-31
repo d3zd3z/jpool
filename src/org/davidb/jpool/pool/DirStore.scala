55e2aae3e21e8133bee6f4a0f2502483b8d25210
David Brown <jpool@davidb.org>
2009-05-30 20:43:40 -0800

    Create dirstore.
    Implement a dirstore that maps more closely to how Posix stores
    directories.  Right now, we're storing the filename in the inode,
    which is messing up hardlinks.	Create a new class, similar to
    the TreeBuilder (using it, in fact) that manages the simplified
    directory we need.  It stores a sorted pairing of filenames and
    the hash of their node information in one or more chunks
    referencable by the Tree walker.
    
    aegis-delta: jpool.1.0.D068
    aegis-uuid: 8248ec8f-317a-404b-b022-80fd306dd697

---
+++ src/org/davidb/jpool/pool/DirStore.scala
@@ -0,0 +1,74 @@
+//////////////////////////////////////////////////////////////////////
+// Dirstore.
+//
+// Whereas the 'Attributes'/Node maps to the information stored in the
+// inode in the filesystem, the Dirstore represents the data stored in
+// a directory.  It is essentially a list of pairs of names and
+// hashes.
+
+package org.davidb.jpool.pool
+
+import java.nio.ByteBuffer
+
+object DirStore {
+  // Iterate (lazily) through the directory specified by the given
+  // hash.
+  def walk(pool: ChunkSource, hash: Hash): Stream[(String, Hash)] = {
+    Stream.concat(TreeBuilder.walk("dir", pool, hash).map(decode _))
+  }
+
+  // This is a bit fragile, since the stream walks through a mutable
+  // structure.
+  private def decode(chunk: Chunk): Stream[(String, Hash)] = decodeData(chunk.data)
+
+  private def decodeData(data: ByteBuffer): Stream[(String, Hash)] = {
+    if (data.remaining == 0)
+      Stream.empty
+    else {
+      val nameLength = data.getShort
+      val nameEncoded = new Array[Byte](nameLength)
+      data.get(nameEncoded)
+      val name = new String(nameEncoded, "iso8859-1")
+      val hashBuf = new Array[Byte](Hash.HashLength)
+      data.get(hashBuf)
+      val hash = Hash.raw(hashBuf)
+      Stream.cons((name, hash), decodeData(data))
+    }
+  }
+}
+
+class DirStore (pool: ChunkStore, limit: Int) {
+  private val builder = TreeBuilder.makeBuilder("dir", pool, limit)
+  private var buffer = ByteBuffer.allocate(limit)
+
+  def append(name: String, hash: Hash) {
+    if (name exists (ch => ch.toInt > 255))
+      error("Encoding error, encountered name with non 8-bit encoded value")
+    val encoded = name.getBytes("iso8859-1")
+    val elength = encoded.length
+    require (elength > 0 && elength <= java.lang.Short.MAX_VALUE)
+    val totalLength = 2 + elength + Hash.HashLength
+    if (buffer.remaining < totalLength)
+      ship
+    if (buffer.remaining < totalLength)
+      error("DirStore buffer size is insufficient for filename length")
+    buffer.putShort(elength.toShort)
+    buffer.put(encoded)
+    buffer.put(hash.getBytes)
+  }
+
+  def finish(): Hash = {
+    ship
+    builder.finish()
+  }
+
+  private def ship {
+    if (buffer.position > 0) {
+      buffer.flip()
+      // printf("Shipping DirStore buffer%n")
+      // Pdump.dump(buffer)
+      builder.add(Chunk.make("dir ", buffer))
+      buffer = ByteBuffer.allocate(limit)
+    }
+  }
+}
---
+++ test/org/davidb/jpool/pool/DirStoreSuite.scala
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////
+// Testing of DirStore.
+
+package org.davidb.jpool.pool
+
+import org.scalatest.Suite
+
+class DirStoreSuite extends Suite with PoolTest {
+
+  def testDirStore {
+    val store = new DirStore(pool, 256*1024)
+    def nums = Stream.concat(Stream.range(1, 256), Stream.range(256, 32768, 256), Stream(32767))
+    for (i <- nums) {
+      val name = StringMaker.generate(i, i)
+      val hash = Hash("blob", name)
+      store.append(name, hash)
+    }
+    val dhash = store.finish()
+
+    var posns = nums
+    for ((name, hash) <- DirStore.walk(pool, dhash)) {
+      val pos = posns.head
+      posns = posns.tail
+      assert(name.length === pos)
+      assert(name === StringMaker.generate(pos, pos))
+      assert(hash === Hash("blob", name))
+    }
+  }
+
+}
