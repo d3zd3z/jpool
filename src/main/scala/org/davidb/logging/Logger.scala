64e4972df41c66cfeb198becde05a3d5abcb6c47
David Brown <jpool@davidb.org>
2010-03-23 08:32:44 -0700

    Remove workaround from Scala 2.7.4 bug.

--- src/main/scala/org/davidb/logging/Logger.scala
+++ src/main/scala/org/davidb/logging/Logger.scala
@@ -19,14 +19,15 @@ trait Logger {
   private var myLogger = log4j.Logger.getLogger(this.getClass().getName())
   Logger.setupLogger
 
-  /* These should all be declared as protected, however this provokes
-   * an occasional bug with Scala 2.7.4 with the varargs. */
-  /*protected*/ def trace(text: String, args: Any*) { myLogger.trace(text format (args : _*)) }
-  /*protected*/ def debug(text: String, args: Any*) { myLogger.debug(text format (args : _*)) }
-  /*protected*/ def info(text: String, args: Any*) { myLogger.info(text format (args : _*)) }
-  /*protected*/ def warn(text: String, args: Any*) { myLogger.warn(text format (args : _*)) }
-  /*protected*/ def logError(text: String, args: Any*) { myLogger.error(text format (args : _*)) }
-  /*protected*/ def fatal(text: String, args: Any*) { myLogger.fatal(text format (args : _*)) }
+  /* If you get strange errors with these methods and are using Scala
+   * version < 2.7.7, comment out the 'protected' declarations to work
+   * around the bug. */
+  protected def trace(text: String, args: Any*) { myLogger.trace(text format (args : _*)) }
+  protected def debug(text: String, args: Any*) { myLogger.debug(text format (args : _*)) }
+  protected def info(text: String, args: Any*) { myLogger.info(text format (args : _*)) }
+  protected def warn(text: String, args: Any*) { myLogger.warn(text format (args : _*)) }
+  protected def logError(text: String, args: Any*) { myLogger.error(text format (args : _*)) }
+  protected def fatal(text: String, args: Any*) { myLogger.fatal(text format (args : _*)) }
 
   // Queries.
   protected def isTraceEnabled: Boolean = myLogger.isTraceEnabled
