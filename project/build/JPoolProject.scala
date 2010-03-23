6ee587a3d1423a94543ffe9f44115af4d66b0cec
David Brown <jpool@davidb.org>
2010-03-23 08:26:51 -0700

    Allow reuse of native code from SBT.
    SBT invokes the test suite using a new class loader each time.  The
    JVM doesn't allow what it thinks is the same shared library to be
    loaded from different classloaders.
    
    To work around this, copy the shared library into a temp file, and use
    this temp file as the library to load.  This might cause a small
    memory leak, but allows the shared lib to be reinitialized and new
    versions tested without having to exit the JVM.

--- project/build/JPoolProject.scala
+++ project/build/JPoolProject.scala
@@ -1,6 +1,7 @@
 import sbt._
 import sbt.FileUtilities._
 import Process._
+import java.util.UUID
 
 class JPoolProject(info: ProjectInfo) extends DefaultProject(info) {
   override def ivyXML =
@@ -18,8 +19,29 @@ class JPoolProject(info: ProjectInfo) extends DefaultProject(info) {
   val codec = "commons-codec" % "commons-codec" % "1.3"
   val h2 = "com.h2database" % "h2" % "1.2.131"
 
+  val libLinux = path("target") / "so" / "liblinux.so"
+
+  // Create temporary copies of the shared library
+  var lastTmpLib: Option[Path] = None
+  def runCleanup {
+    if (lastTmpLib != None) {
+      lastTmpLib.get.asFile.delete
+      lastTmpLib = None
+    }
+  }
+
+  class Cleanup extends Thread {
+    override def run() {
+      runCleanup
+    }
+  }
+  Runtime.getRuntime.addShutdownHook(new Cleanup)
+
   // Compiling native code.  The work is done by a regular makefile.
   lazy val native = task {
+    runCleanup
+
+    val tmpLib = Path.fromFile("/tmp") / (UUID.randomUUID().toString + ".so")
     val cp = runClasspath +++ mainDependencies.scalaJars
     val home = System.getProperty("java.home")
     val p = "make" :: "-f" :: "Makefile.native" ::
@@ -28,14 +50,17 @@ class JPoolProject(info: ProjectInfo) extends DefaultProject(info) {
       "COMPILE_PATH=" + mainCompilePath ::
       Nil
     val status = Process(p) ! log
-    if (status == 0)
+    if (status == 0) {
+      // Create temporary copy of shared library.
+      lastTmpLib = Some(tmpLib)
+      FileUtilities.copyFile(libLinux, tmpLib, log)
+      // The native library will look for this property to load an
+      // explicit path.
+      System.setProperty("linux.lib", tmpLib.absolutePath)
       None
-    else
+    } else
       Some("Make error: " + status)
   } dependsOn(compile)
-  override def testAction = super.testAction dependsOn(native)
+  override def testCompileAction = super.testCompileAction dependsOn(native)
 
-  // The native library will look for this property to load an
-  // explicit path.
-  System.setProperty("linux.lib", (path("target") / "so" / "liblinux.so").absolutePath)
 }
