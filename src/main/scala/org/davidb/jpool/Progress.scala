db307918a5eeb73a8613b5aaf878894963aabe8f
David Brown <jpool@davidb.org>
2010-04-04 01:50:07 -0700

    Rewrite Progress meter code.
    Factor the specific meter out of the general meter handling code.
    The organization is still a bit disorderly, but it's a big step to
    being able to open more than one progress meter at a time.

--- src/main/scala/org/davidb/jpool/Progress.scala
+++ src/main/scala/org/davidb/jpool/Progress.scala
@@ -17,141 +17,20 @@ object NullProgress extends DataProgress {
   def addDup(count: Long) {}
 }
 
-object TheProgress extends Progress
+// A progress meter is anything that can give status periodically.  It
+// should return an array of strings for the lines of the meter.
+trait ProgressMeter {
+  def formatMeter(): Array[String]
 
-object Progress {
-  def open(): Progress = {
-    TheProgress.open()
-    TheProgress
-  }
+  // It can expect to be able to call this update method to inform the
+  // meter framework that the meter is potentially changed.
+  protected def update()
 }
 
-// Don't try creating more than one of these.
-class Progress extends AnyRef with Loggable with DataProgress {
-  // API used by things writing data.
-
-  // Ordinary data written (as in chunk data).
-  def addData(count: Long) = synchronized { state.data += count; check }
-
-  // Dedupped data.
-  def addDup(count: Long) = synchronized { state.dup += count; check }
-
-  // Data entirely skipped (file is already known).
-  def addSkip(count: Long) = synchronized { state.skip += count; check }
-
-  // Indicate files added.
-  def addNode() { addNode(1) }
-  def addNode(count: Long) = synchronized { state.nodes += count; check }
-
-  def reset() {
-    state.reset()
-    nextUpdate = None
-  }
-
-  def open() = synchronized {
-    if (opened)
-      error("Progress meter already opened")
-    opened = true
-    Logger.pushWrapper(logTag, logMessage _)
-  }
-  def close() = synchronized {
-    if (!opened)
-      error("Progress meter not opened")
-    show(true)
-    nextUpdate = None
-    linesPrinted = 0
-    opened = false
-    Logger.popWrapper(logTag)
-  }
-
-  // Information display.
-  abstract class Info {
-    // Return an info display consisting of an array of lines to
-    // display the information.
-    def getInfo: Array[String]
-  }
-
-  class BackupInfo {
-    var data = 0L
-    var dup = 0L
-    var skip = 0L
-    var nodes = 0L
-    def getInfo: Array[String] = {
-      Array("[data: %s, dup, %s, skip: %s, total: %s, nodes: %s]" format(
-        humanize(data), humanize(dup), humanize(skip),
-        humanize(data + dup + skip),
-        nodes))
-    }
-    def reset() {
-      data = 0L
-      dup = 0L
-      skip = 0L
-      nodes = 0L
-    }
-  }
-  private val state = new BackupInfo
-
-  //////////////////////////////////////////////////////////////////////
-  // Implementation.
-
-  private var opened = false
-  private val stdout = System.console.writer
-
-  private def check {
-    if (!opened)
-      error("Progress meter has not been opened")
-    if (timeForUpdate()) {
-      show()
-    }
-  }
-
-  // Called by the logger to wrap messages.
-  private val logTag = new Object
-  private def logMessage(thunk: => Unit) = synchronized {
-    val oldLines = linesPrinted
-    if (oldLines > 0)
-      clear
-    thunk
-    if (oldLines > 0)
-      show(true)
-  }
-
-  private var linesPrinted = 0
-  // Print the update nicely.
-  private def clear() = synchronized {
-    if (linesPrinted > 0) {
-      Console.printf("\033[%dA\033[J", linesPrinted)
-      linesPrinted = 0
-    }
-  }
-
-  def show() { show(false) }
-  def show(force: Boolean) = synchronized {
-    clear()
-    val lines = state.getInfo
-    for (line <- lines)
-      Console.printf("%s\n", line)
-    linesPrinted = lines.length
-    Console.flush()
-  }
-
-  private var nextUpdate: Option[Long] = None
-
-  // Is it time for the next update?
-  private def timeForUpdate(): Boolean = {
-    val now = new Date().getTime()
-    nextUpdate match {
-      case Some(when) if (now < when) =>
-        false
-      case _ =>
-        nextUpdate = Some(now + 1000L)
-        nextUpdate != None
-    }
-  }
-
+object ProgressMeter {
   // Convert a large number into a nice human readable format.
   // Largest result would be 1023.9GiB or 9 characters.
-  private def humanize(num: Double): String = {
+  def humanize(num: Double): String = {
     var answer = num
     var unit = units
     while (answer > 1024.0) {
@@ -160,5 +39,111 @@ class Progress extends AnyRef with Loggable with DataProgress {
     }
     "%6.1f%s" format (answer, unit.head)
   }
-  private val units = List("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+  val units = List("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+}
+
+// The Progress object drives the progress meter.
+object Progress { prog =>
+  def open(): Progress = synchronized {
+    if (meter ne null)
+      error("Progress meter already opened")
+    Logger.pushWrapper(logTag, logMessage _)
+    val m = new BackupMeter
+    meter = m
+    m
+  }
+  def close() = synchronized {
+    if (meter eq null)
+      error("Progress meter not opened")
+    show(true)
+    meter = null
+    Logger.popWrapper(logTag)
+  }
+
+  class BackupMeter extends BackupProgressMeter {
+    // TODO: How to make these available.
+    def show() = prog.show()
+    def close() = prog.close()
+    def update() = prog.update()
+  }
+
+  trait Updater extends ProgressMeter {
+    def update() { prog.update() }
+  }
+  private var meter: ProgressMeter = null
+
+  private val stdout = System.console.writer // TODO: redirected stdout.
+  private val logTag = new Object
+  private def logMessage(thunk: => Unit) = synchronized {
+    val oldLines = linesPrinted
+    clear()
+    thunk
+    if (oldLines > 0)
+      show(true)
+  }
+
+  private def update() {
+    if (timeForUpdate())
+      show()
+  }
+  private var linesPrinted = 0
+  private def clear() = synchronized {
+    if (linesPrinted > 0) {
+      Console.printf("\033[%dA\033[J", linesPrinted)
+      linesPrinted = 0
+    }
+  }
+  private def show() { show(false) }
+  private def show(force: Boolean) = synchronized {
+    clear()
+    val lines = meter.formatMeter()
+    for (line <- lines)
+      Console.printf("%s\n", line)
+    linesPrinted = lines.length
+    Console.flush()
+  }
+
+  private var nextUpdate = new Date().getTime() + 1000
+  private def timeForUpdate(): Boolean = {
+    val now = new Date().getTime()
+    val doit = (now >= nextUpdate)
+    if (doit)
+      nextUpdate = now + 1000
+    doit
+  }
+}
+
+trait Progress extends DataProgress {
+  def addSkip(count: Long)
+  def addNode(count: Long)
+  def addNode() { addNode(1) }
+  def close()
+  def reset()
+  def show()
+}
+
+// Backup data progress meter.
+abstract class BackupProgressMeter extends ProgressMeter with Progress {
+  import ProgressMeter.humanize
+
+  def addData(count: Long) = synchronized { data += count; update() }
+  def addDup(count: Long) = synchronized { dup += count; update() }
+  def addSkip(count: Long) = synchronized { skip += count; update() }
+  def addNode(count: Long) = synchronized { nodes += count; update() }
+  def reset() = synchronized {
+    data = 0L
+    dup = 0L
+    skip = 0L
+    nodes = 0L
+  }
+
+  private var data = 0L
+  private var dup = 0L
+  private var skip = 0L
+  private var nodes = 0L
+  def formatMeter() = Array(
+    "[data: %s, dup: %s, skip: %s, total: %s, nodes: %s]" format(
+      humanize(data), humanize(dup), humanize(skip),
+      humanize(data + dup + skip),
+      nodes))
 }
