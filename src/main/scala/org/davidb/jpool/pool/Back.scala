f7a188e3461e6c2d6aab993c2b6196d6cd6bfda1
David Brown <jpool@davidb.org>
2012-03-12 22:38:16 -0700

    Convert Back record to use Attributes
    Instead of the java properties format, convert the backup record to
    the same as the Attributes, with a kind of 'back'.  It is still able
    to decode property entries as needed.

--- src/main/scala/org/davidb/jpool/pool/Back.scala
+++ src/main/scala/org/davidb/jpool/pool/Back.scala
@@ -7,29 +7,49 @@ package pool
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 import java.nio.ByteBuffer
 import java.util.Properties
+import collection.JavaConversions._
 
 object Back {
   // Retrieve the properties associated with this backup.
   def load(pool: ChunkSource, hash: Hash): Back = {
     val chunk = pool(hash)
     val data = chunk.data
-    val encoded = new ByteArrayInputStream(data.array, data.arrayOffset + data.position, data.remaining)
-    val props = new Properties
-    props.loadFromXML(encoded)
-    new Back(props)
+    if (data.get(data.position) == '<') {
+      val encoded = new ByteArrayInputStream(data.array, data.arrayOffset + data.position, data.remaining)
+      val props = new Properties
+      props.loadFromXML(encoded)
+      new Back(props)
+    } else {
+      val atts = Attributes.decode(chunk)
+      if (atts.kind != "back")
+	sys.error("Backup record is not of type \"back\"")
+      val props = new Properties
+      for ((key, value) <- atts.contents)
+	props.setProperty(key, value)
+      new Back(props)
+    }
   }
 }
 
 class Back(val props: Properties) {
   // Write the properties, encoded to a chunk in the specified pool,
   // returning the hash describing this chunk.
-  def store(pool: ChunkStore): Hash = {
+  def storeXml(pool: ChunkStore): Hash = {
     val buf = new ByteArrayOutputStream
     props.storeToXML(buf, "Backup")
     val encoded = ByteBuffer.wrap(buf.toByteArray)
     val chunk = Chunk.make("back", encoded)
     pool += (chunk.hash -> chunk)
     chunk.hash
+  }
+
+  def store(pool: ChunkStore): Hash = {
+    var map = Map.empty[String, String]
+    for (key <- props.stringPropertyNames()) {
+      val value = props.getProperty(key)
+      map += (key -> value)
+    }
+    new Attributes("back", map).store(pool, "back")
   }
 
   // Retrieve the hash property, as a hash.
